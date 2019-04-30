51ec3f94a8098ffc7185646d51b90bde2ba6e3e6
Osamah <osamah@aldoaiss.de>
2019-04-30 09:40:59 +0200

    Improvements

---
+++ src/fonts/index.js
@@ -0,0 +1,30 @@
+import MetropolisBoldEOT from './Metropolis-Bold.eot';
+import MetropolisSemiBoldEOT from './Metropolis-SemiBold.eot';
+import MetropolisRegularEOT from './Metropolis-Regular.eot';
+
+import MetropolisBoldTTF from './Metropolis-Bold.ttf';
+import MetropolisSemiBoldTTF from './Metropolis-SemiBold.ttf';
+import MetropolisRegularTTF from './Metropolis-Regular.ttf';
+
+import MetropolisBoldWOFF from './Metropolis-Bold.woff';
+import MetropolisSemiBoldWOFF from './Metropolis-SemiBold.woff';
+import MetropolisRegularWOFF from './Metropolis-Regular.woff';
+
+import MetropolisBoldWOFF2 from './Metropolis-Bold.woff2';
+import MetropolisSemiBoldWOFF2 from './Metropolis-SemiBold.woff2';
+import MetropolisRegularWOFF2 from './Metropolis-Regular.woff2';
+
+export {
+  MetropolisRegularEOT,
+  MetropolisSemiBoldEOT,
+  MetropolisBoldEOT,
+  MetropolisRegularTTF,
+  MetropolisSemiBoldTTF,
+  MetropolisBoldTTF,
+  MetropolisRegularWOFF,
+  MetropolisSemiBoldWOFF,
+  MetropolisBoldWOFF,
+  MetropolisRegularWOFF2,
+  MetropolisSemiBoldWOFF2,
+  MetropolisBoldWOFF2,
+};
