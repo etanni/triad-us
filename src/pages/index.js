ef5c2fb4f02dcc4b24a6aaff95e780d5b70f4249
Osamah <osamah@aldoaiss.de>
2019-05-24 10:30:16 +0200

    Update stuff from weekend

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,12 @@ import {
   Seo,
   SectionTitle,
   Hero,
-  // ImageGridTwoLargeFourSmall,
-  // LargeChild,
-  // SmallChild,
   Button,
   PageLayout,
 } from 'gatsby-theme-shopify-poulo';
 
-// import ImageGridContent from '../components/ImageGridContent';
 import WhiteLogo from '../images/white-logo-circle.png';
 import ScrollLogo from '../images/scroll-logo.png';
-// import LargeBG1 from '../images/large-bg-1.png';
-// import LargeBG2 from '../images/large-bg-2.png';
-// import SmallBG1 from '../images/small-bg-1.png';
-// import SmallBG2 from '../images/small-bg-2.png';
-// import SmallBG3 from '../images/small-bg-3.png';
-// import SmallBG4 from '../images/small-bg-4.png';
 import { Promotion } from '../hero-bgs/Promotion';
 
 export default ({ data: { promotionBG, allShopifyProduct } }) => {
@@ -45,48 +35,6 @@ export default ({ data: { promotionBG, allShopifyProduct } }) => {
       >
         <Button type="button">Shop All Men</Button>
       </Hero>
-      {/* <Section fullWidth>
-        <SectionTitle noDescription>
-          With breathable Seamless construction, our Swiftly collection
-          minimizes chafing, maximizes comfort.
-        </SectionTitle>
-        <ImageGridTwoLargeFourSmall fullWidth>
-          <LargeChild
-            background={`url(${LargeBG1}) center bottom/cover no-repeat`}
-            hoverColor="#aaaaaa22"
-            hasHover
-          >
-            <ImageGridContent
-              title="TOPS"
-              description="soft & cozy"
-              button={<Button>Shop Tops</Button>}
-            />
-          </LargeChild>
-          <LargeChild
-            background={`url(${LargeBG2}) center bottom/cover no-repeat`}
-            hoverColor="#aaaaaa22"
-            hasHover
-          >
-            <ImageGridContent
-              title="BOTTOMS"
-              description="light & breezy"
-              button={<Button>Shop Bottoms</Button>}
-            />
-          </LargeChild>
-          <SmallChild
-            background={`url(${SmallBG1}) center bottom/cover no-repeat`}
-          />
-          <SmallChild
-            background={`url(${SmallBG2}) center bottom/cover no-repeat`}
-          />
-          <SmallChild
-            background={`url(${SmallBG3}) center bottom/cover no-repeat`}
-          />
-          <SmallChild
-            background={`url(${SmallBG4}) center bottom/cover no-repeat`}
-          />
-        </ImageGridTwoLargeFourSmall>
-      </Section> */}
       <Section>
         <Hero
           fullWidth
