1420cb90ff5bf4940be0f2290d3e2c9f6340d41b
Osamah <osamah@aldoaiss.de>
2019-06-09 11:05:30 +0200

    MobileMegaMenu

---
+++ src/gatsby-theme-shopify-poulo/components/MobileMegaMenu/index.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import get from 'lodash/get';
+import find from 'lodash/find';
+
+import {
+  Wrapper,
+  CategoryWrapper,
+  BottomCategoryWrapper,
+  Title,
+  Category,
+  MegaMenuProduct,
+} from '../../../styles/MobileMegaMenuStyles';
+
+const Content = ({ activeDisplayName }) => {
+  const {
+    allShopifyProduct: { edges: products },
+  } = useStaticQuery(graphql`
+    query {
+      allShopifyProduct {
+        edges {
+          node {
+            id
+            shopifyId
+            handle
+            title
+            description
+            tags
+            priceRange {
+              minVariantPrice {
+                amount
+                currencyCode
+              }
+              maxVariantPrice {
+                amount
+                currencyCode
+              }
+            }
+            images {
+              localFile {
+                childImageSharp {
+                  fixed(width: 200) {
+                    src
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const tops = [
+    'thrive-tshirt',
+    'thrive-t-shirt-green',
+    'conquer-tank-black',
+    'conquer-full-zip-hoodie-black',
+    '101-nike',
+    '103-wpn',
+  ];
+  const bottoms = ['thrive-tshirt', 'thrive-t-shirt-green'];
+
+  return (
+    <Wrapper>
+      <Title marginMultiplier={2}>TOPS</Title>
+      <Category>
+        {tops.map(handle => {
+          const product = find(products, ({ node }) => handle === node.handle);
+          if (!product) return null;
+
+          const {
+            node,
+            node: { title, images },
+          } = product;
+          return (
+            <MegaMenuProduct
+              key={node.handle}
+              handle={node.handle}
+              title={title}
+              image={get(images, '0.localFile.childImageSharp.fixed.src')}
+            />
+          );
+        })}
+      </Category>
+      <BottomCategoryWrapper>
+        <CategoryWrapper>
+          <Title marginMultiplier={2}>BOTTOMS</Title>
+          <Category>
+            {bottoms.map(handle => {
+              const product = find(
+                products,
+                ({ node }) => handle === node.handle
+              );
+              if (!product) return null;
+
+              const {
+                node,
+                node: { title, images },
+              } = product;
+              return (
+                <MegaMenuProduct
+                  key={node.handle}
+                  handle={node.handle}
+                  title={title}
+                  image={get(images, '0.localFile.childImageSharp.fixed.src')}
+                />
+              );
+            })}
+          </Category>
+        </CategoryWrapper>
+        <CategoryWrapper>
+          <Title marginMultiplier={2}>ACCESSORIES</Title>
+          <Category>
+            {bottoms.map(handle => {
+              const product = find(
+                products,
+                ({ node }) => handle === node.handle
+              );
+              if (!product) return null;
+
+              const {
+                node,
+                node: { title, images },
+              } = product;
+              return (
+                <MegaMenuProduct
+                  key={node.handle}
+                  handle={node.handle}
+                  title={title}
+                  image={get(images, '0.localFile.childImageSharp.fixed.src')}
+                />
+              );
+            })}
+          </Category>
+        </CategoryWrapper>
+      </BottomCategoryWrapper>
+    </Wrapper>
+  );
+};
+
+export default Content;
---
+++ src/styles/MobileMegaMenuStyles.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Paragraph, PrimaryTitle } from 'gatsby-theme-shopify-poulo';
+import { Link } from 'gatsby';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  a {
+    color: black;
+    text-decoration: none;
+  }
+`;
+export const CategoryWrapper = styled.div`
+  /* margin-right: 16px; */
+  width: 100%;
+`;
+export const BottomCategoryWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+export const Category = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  > * {
+    width: 50%;
+    @media (min-width: 484px) {
+      width: calc(100% / 3);
+    }
+  }
+`;
+export const Title = styled(PrimaryTitle)`
+  max-width: initial;
+  margin-left: 12px;
+  text-align: left;
+`;
+
+export const ProductWrapper = styled(Link)`
+  text-align: center;
+  padding: 0 12px 16px;
+`;
+export const ProductImage = styled.img`
+  width: 100%;
+  height: 160px;
+  margin: 0;
+  object-fit: cover;
+`;
+export const ProductTitle = styled(Paragraph)`
+  font-weight: 600;
+`;
+
+export const MegaMenuProduct = ({ image, title, handle }) => {
+  return (
+    <ProductWrapper to={`/product/${handle}`}>
+      <ProductImage src={image} alt={title} />
+      <ProductTitle>{title}</ProductTitle>
+    </ProductWrapper>
+  );
+};
